85ac36b3b04e1cf0f7bb45f39a7acb8c5e2e3cb3
James Chapman <james.chapman@umusic.com>
2022-07-15 10:13:14 +0100

    Update Tests.cs
    Fix build

--- EfCore.Audit.UnitTests/Tests.cs
+++ EfCore.Audit.UnitTests/Tests.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using EfCore.Audit.Party.Domain;
 using EfCore.Audit.UnitTests.Models;
 using Microsoft.EntityFrameworkCore;
 using Moq;
